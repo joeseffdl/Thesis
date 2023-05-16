c01bef7f77c362f94ab1efb3f550ea65bb0030c5
Joseph De Leon <joeseffdl@gmail.com>
2023-05-17 01:56:41 +0800

    Applied next/font, to fix orientation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import { Navigation } from "./components"
 import "./globals.css"
+import { Poppins, Montserrat } from "next/font/google"
+
+const poppins = Poppins({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  fallback: ["system-ui", "arial"],
+})
 
 export const metadata = {
   title: "Smart Hard Hat System",
@@ -14,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className="flex">
+      <body
+        className={`flex ${poppins.variable} font-poppins`}
+      >
         <Navigation />
         {children}
       </body>
