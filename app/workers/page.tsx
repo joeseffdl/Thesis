73b940d813bae016a823ef3283d7a67f68360ab7
Joseph De Leon <joeseffdl@gmail.com>
2023-05-28 13:39:10 +0800

    Display data from database to Web App

--- app/workers/page.tsx
+++ app/workers/page.tsx
@@ -1,17 +1,39 @@
+"use client"
+
 import { WorkerCard, WorkerHeader } from "./";
+import { ref, onValue, set } from "firebase/database";
+import { db } from "../../utils/firebase";
+import { useState, useEffect } from "react";
+
+type WorkerProps = {
+  name: string;
+  timeIn: string;
+  timeOut: string;
+}
 
 export default function Workers() {
+  const [workersList, setWorkersList] = useState([]);
+
+  useEffect(() => {
+    const workersRef = ref(db, "workersList");
+    onValue(workersRef, (snapshot) => {
+      const data = snapshot.val();
+      if(data !== null) {
+        Object.values(data).map((worker) => {
+          setWorkersList((prev) => [...prev, worker] as any);
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className="w-full">
       <div className="p-5">
         <WorkerHeader />
         <div className="flex flex-col gap-2 lg:grid lg:grid-cols-3">
-          <WorkerCard name={"Worker 1"} timeIn="9:00 AM" timeOut="5:00 PM" />
-          <WorkerCard name={"Worker 2"} timeIn="9:00 AM" timeOut="5:00 PM" />
-          <WorkerCard name={"Worker 3"} timeIn="9:00 AM" timeOut="5:00 PM" />
-          <WorkerCard name={"Worker 4"} timeIn="9:00 AM" timeOut="5:00 PM" />
-          <WorkerCard name={"Worker 5"} timeIn="9:00 AM" timeOut="5:00 PM" />
-          <WorkerCard name={"Worker 6"} timeIn="9:00 AM" timeOut="5:00 PM" />
+          {workersList.map((worker:WorkerProps) => (
+            <WorkerCard key={worker.name} name={worker.name} timeIn={worker.timeIn} timeOut={worker.timeOut} />
+          ))}
         </div>
       </div>
     </div>
