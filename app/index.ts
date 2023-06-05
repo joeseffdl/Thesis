f3838dfd73b4b90027c953c4573709e2a5ba0af7
Joseph De Leon <joeseffdl@gmail.com>
2023-06-05 18:17:47 +0800

    Logging out logic and created component for dashboard

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,7 @@
 import MainCard from "./MainCard";
 import MainContent from "./MainContent";
 import ProjectCard from "./ProjectCard";
+import Dashboard from "./Dashboard";
 
-export { MainContent, ProjectCard, MainCard };
+
+export { MainContent, ProjectCard, MainCard, Dashboard };
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
-import { MainContent } from ".";
-import { Header, SubHeader } from "./components";
+"use client"
 
-export default function Dashboard() {
+import { Dashboard } from "./";
+import { auth } from "../utils/firebase"
+import { useAuthState } from "react-firebase-hooks/auth"
+import {  useEffect } from "react"
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter()
+
+  const [user, loading] = useAuthState(auth)
+  
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/login")
+    }
+  }, [user, loading])
+
   return (
     <main className="w-full">
-      <section className="bg-slate-200 p-8">
-        <Header title="Dashboard" subtitle="Greetings, Super Admin!" />
-        <SubHeader />
-      </section>
-      <section className="p-8 flex flex-col lg:flex-row gap-5">
-        <MainContent />
-      </section>
+      <Dashboard />
     </main>
   );
 }
