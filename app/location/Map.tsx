c0531489153bbaba20206c5af3d9240d8531148f
Joseph De Leon <joeseffdl@gmail.com>
2023-06-20 02:11:13 +0800

    Added value for the location of GPS

--- app/location/Map.tsx
+++ app/location/Map.tsx
@@ -4,22 +4,38 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+import { useContext } from "react"
+import { DataContext } from "@/utils/context"
 
 const Map = () => {
+  const { firebaseData } = useContext(DataContext)
+
+  const longitude = firebaseData.map((data) => data.longitude)
+  const latitude = firebaseData.map((data) => data.latitude)
+
   return (
     <MapContainer
       style={{ width: "100%", height: "100%" }}
-      center={[14.576215, 121.181849]}
-      zoom={13}
+      center={[14.574271167, 121.001043333]}
+      zoom={200}
       scrollWheelZoom={true}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[14.576215, 121.181849]}>
-        <Popup>Bahay ni Per</Popup>
-      </Marker>
+      {firebaseData.map((data) => {
+        return (
+          <Marker
+            key={data.id}
+            position={[data.latitude || 0, data.longitude || 0]}
+          >
+            <Popup className={`${data.status === "danger" ? "bg-red-200": data.status === "warning" ? "bg-amber-200" : "bg-green-200"}`}>
+              <h1 className="font-bold uppercase">{data.name}</h1>
+            </Popup>
+          </Marker>
+        )
+      })}
     </MapContainer>
   )
 }
